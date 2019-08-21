f3981041ef6cac0f871f715be3069dbe3f2d97c0
rodrigo.bisterco <rodrigo.bisterco@BRZBEL000671L.ad.infosys.com>
2019-08-21 14:57:57 -0300

    consumindo API meu usuario

---
+++ FirstAPI/github_list_repos2.py
@@ -0,0 +1,30 @@
+import json
+import requests
+
+'''
+Consumindo API do github - teste meu usuario:
+https://api.github.com/users/bstk0
+'''
+api_url_base = 'https://api.github.com/'
+headers = {'Content-Type': 'application/json',
+           'User-Agent': 'Python Student',
+           'Accept': 'application/vnd.github.v3+json'}
+
+def get_repos(username):
+
+    api_url = '{}users/{}'.format(api_url_base, username)
+
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        return (response.content)
+    else:
+        print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url))
+        return None
+
+repo_list = get_repos('bstk0')
+
+if repo_list is not None:
+    print(repo_list)
+else:
+    print('No Repo List Found')    
