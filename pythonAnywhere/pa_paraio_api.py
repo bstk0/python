d6bad137937d8b0e7a20dd767c1951443709e77c
bstk0 <rodrigo.bisterco@gmail.com>
2021-09-08 16:48:04 -0300

    paginacao ok

--- pythonAnywhere/pa_paraio_api.py
+++ pythonAnywhere/pa_paraio_api.py
@@ -1,4 +1,5 @@
 import requests
+from flask import request
 from flask import Blueprint
 from flask import jsonify
 #from global_ import Global
@@ -10,11 +11,12 @@ paraio_api = Blueprint('paraio_api', __name__)
 
 @paraio_api.route("/movim") 
 def buscar_dados_movim():
-    
+    dias = request.args.get('dias', default='10', type = str)
     auth = {'Authorization' : 'Anonymous app:dbapp' }    
-    request = requests.get("http://paraio.com/v1/movims?sort=timestamp&limit=10",headers=auth);
+    returnRequest = requests.get("http://paraio.com/v1/movims?sort=timestamp&limit="+dias ,headers=auth);
     #print(request.content);
-    return jsonify(request.content)
+    #return jsonify(request.content)
+    return returnRequest.content
     
     
 @paraio_api.route("/saldo") 
@@ -23,7 +25,8 @@ def buscar_dados_saldo():
     auth = {'Authorization' : 'Anonymous app:dbapp' }    
     request = requests.get("https://paraio.com/v1/saldo/1143180213983645696",headers=auth);
     #print(request.content);
-    return jsonify(request.content)
+    #return jsonify(request.content)
+    return request.content
     
 #if __name__ == '__main__':
 #    buscar_dados_movim()
