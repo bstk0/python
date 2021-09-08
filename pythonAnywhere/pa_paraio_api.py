f8939e9b436c18f12dc1da466125dd41d3735a26
bstk0 <rodrigo.bisterco@gmail.com>
2021-09-08 18:32:03 -0300

    agora atualizando saldo

--- pythonAnywhere/pa_paraio_api.py
+++ pythonAnywhere/pa_paraio_api.py
@@ -9,7 +9,7 @@ from flask import jsonify
 
 paraio_api = Blueprint('paraio_api', __name__)
 
-@paraio_api.route("/movim") 
+@paraio_api.route("/movim", methods=['GET']) 
 def buscar_dados_movim():
     dias = request.args.get('dias', default='10', type = str)
     auth = {'Authorization' : 'Anonymous app:dbapp' }    
@@ -17,9 +17,18 @@ def buscar_dados_movim():
     #print(request.content);
     #return jsonify(request.content)
     return returnRequest.content
+
+@paraio_api.route("/movim", methods=['POST']) 
+def insert_dados_movim():
+    data = request.get_json();
+    auth = {'Authorization' : 'Anonymous app:dbapp','Content-Type' : 'application/json' }    
+    returnRequest = requests.post("https://paraio.com/v1/movims/", json=data, headers=auth, verify=False);
+    #print(request.content);
+    #return jsonify(request.content)
+    return str(returnRequest.status_code)
+
     
-    
-@paraio_api.route("/saldo") 
+@paraio_api.route("/saldo", methods=['GET']) 
 def buscar_dados_saldo():
     
     auth = {'Authorization' : 'Anonymous app:dbapp' }    
@@ -27,7 +36,18 @@ def buscar_dados_saldo():
     #print(request.content);
     #return jsonify(request.content)
     return request.content
-    
+
+@paraio_api.route("/saldo", methods=['PUT']) 
+def update_saldo():
+    data = request.get_json();
+    auth = {'Authorization' : 'Anonymous app:dbapp' }    
+    r = requests.put("https://paraio.com/v1/saldo/1143180213983645696",json=data  ,headers=auth);
+    #print(request.content);
+    #return jsonify(request.content)
+    return str(r.status_code)
+
+
+
 #if __name__ == '__main__':
 #    buscar_dados_movim()
     
