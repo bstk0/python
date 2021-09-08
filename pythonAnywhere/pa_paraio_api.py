361056925b5d23bf67c18f5a6841ad2e99b4940a
bstk0 <rodrigo.bisterco@gmail.com>
2021-09-08 16:17:23 -0300

    adicionando saldo

--- pythonAnywhere/pa_paraio_api.py
+++ pythonAnywhere/pa_paraio_api.py
@@ -16,7 +16,15 @@ def buscar_dados_movim():
     #print(request.content);
     return jsonify(request.content)
     
+    
+@paraio_api.route("/saldo") 
+def buscar_dados_saldo():
+    
+    auth = {'Authorization' : 'Anonymous app:dbapp' }    
+    request = requests.get("https://paraio.com/v1/saldo/1143180213983645696",headers=auth);
+    #print(request.content);
+    return jsonify(request.content)
+    
 #if __name__ == '__main__':
 #    buscar_dados_movim()
-    
     
